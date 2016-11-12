e8371ca240f0fdb66be01b9c4a3566a1e3db69fb
kekebeen <kakosi69@gmail.com>
2016-11-12 17:06:14 +0100

    Added db scripts

--- flask_flashcards/app/__init__.py
+++ flask_flashcards/app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 #load config csrf protection for wtf forms and secret key
 app.config.from_object('config')
+#initialize db
+db = SQLAlchemy(app)
 
 from app import views
