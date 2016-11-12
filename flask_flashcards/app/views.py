22a7ca8b8a8bf6b556650de5b6763e9578d69a26
kekebeen <kakosi69@gmail.com>
2016-11-12 14:44:14 +0100

    Added base layout

--- flask_flashcards/app/views.py
+++ flask_flashcards/app/views.py
@@ -1,6 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Heelo World"
+    user = {'nickname' : 'admin'} #fake user
+    return render_template('index.html',
+                    title = 'Home',
+                    user = user)
