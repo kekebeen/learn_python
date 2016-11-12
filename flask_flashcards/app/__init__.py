857d5f7a2db57ab65a78eba3c49a3eecbc0a1c71
kekebeen <kakosi69@gmail.com>
2016-11-12 16:07:40 +0100

    Added basic form route and validation

--- flask_flashcards/app/__init__.py
+++ flask_flashcards/app/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
+#load config csrf protection for wtf forms and secret key
+app.config.from_object('config')
+
 from app import views
--- flask_flashcards/app/views.py
+++ flask_flashcards/app/views.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +9,11 @@ def index():
     return render_template('index.html',
                     title = 'Home',
                     user = user)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for Openid="%s", remember me:"%s" ' % (form.openid.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',title='Sign in',form = form)
