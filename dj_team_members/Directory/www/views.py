5560b84dd16111b799a24060b06f71a02cce58a2
kekebeen <kakosi69@gmail.com>
2016-11-20 12:58:54 +0100

    Added new project

--- dj_team_members/Directory/www/views.py
+++ dj_team_members/Directory/www/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from .models import Person
 # Create your views here.
 def index(request):
-    return render(request,'index.html');
+    #get all the piipl
+    people = Person.objects.all()
+    return render(request,'index.html', {'people':people});
