e0bfb42e6e6413585aa10a8cb8194adf3bd05bb2
Benjamin Babic <kekebeen@users.noreply.github.com>
2017-12-10 14:57:53 +0100

    Update login.py

--- automation/login.py
+++ automation/login.py
@@ -1,10 +1,13 @@
 from page import Page
 
 c = Page('chrome')
+f = Page('firefox')
+
 loginButtonXpath = "//input[@value='Log In']"
 
 c.find_by_id_and_click("email", "test@mail.com")
 c.find_by_id_and_click("pass", "test")
 c.button_by_xpath_click(loginButtonXpath)
 c.tearDown()
+
 
